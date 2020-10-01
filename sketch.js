edd7b6415dc9582d31580d13dc08d02af483954d
sreeneevedhan <71315011+sreeneevedhan@users.noreply.github.com>
2020-10-01 10:47:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,11 +23,11 @@ function draw() {
 
   var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
   if(damage>10){
-bullet.shapeColor=color(225,0,0);
+wall.shapeColor=color(225,0,0);
 }
 if(damage<10){
 
-  bullet.shapeColor=color(0,255,0);
+  wall.shapeColor=color(0,255,0);
 }
  }
   drawSprites();
